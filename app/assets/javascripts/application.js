5fbb40c26c23eccec21b19f45531c0c0faea3e36
App Academy Student <student@appacademy.io>
2014-03-21 15:51:25 -0700

    working on backbone result view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,5 +16,13 @@
 //= require turbolinks
 //= require bootstrap
 
+//= require underscore
+//= require backbone
+//= require sql_pql
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
 
---
+++ app/assets/javascripts/sql_pql.js
@@ -0,0 +1,13 @@
+window.SqlPql = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  SqlPql.initialize();
+});
